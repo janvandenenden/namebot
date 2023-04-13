254dea3e9d5701dfd038238454ce09f687b49f3a
Jan Van den Enden <jan@enden.be>
2023-04-13 10:46:53 +0200

    create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="py-3 container text-center text-sm mx-auto">&copy; NameGenius.ai {new Date().getFullYear()}</div>
+  )
+}
+
+export default Footer
